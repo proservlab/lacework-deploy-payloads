754b52801424531ab117c613353e476c26ec6051
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-02 20:40:50 -0700

    update payloads

--- linux/run_me.sh
+++ linux/run_me.sh
@@ -3,5 +3,13 @@
 # variable from jinja2 template 
 ENVIRONMENT="%%{ environment }%%"
 DEPLOYMENT="%%{ deployment }%%"
+ATTACKER_INSTANCES="%%{ attacker_instances }%%"
+TARGET_INSTANCES="%%{ target_instances }%%"
+ATTACKER_K8S_SERVICES="%%{ attacker_k8s_services }%%"
+TARGET_K8S_SERVICES="%%{ target_k8s_services }%%"
 
 echo "${ENVIRONMENT}:${DEPLOYMENT}" > /tmp/run_me.log
+echo "${ATTACKER_INSTANCES}" | base64 -d > /tmp/attacker_instances.log
+echo "${TARGET_INSTANCES}" | base64 -d > /tmp/target_instances.log
+echo "${ATTACKER_K8S_SERVICES}" | base64 -d >> /tmp/attacker_k8s_services.log
+echo "${TARGET_K8S_SERVICES}" | base64 -d >> /tmp/target_k8s_services.log
--- windows/run_me.ps1
+++ windows/run_me.ps1
@@ -1,5 +1,13 @@
 # variable from jinja2 template 
 $environment = "%%{ environment }%%"
-$deployment = "%%{ deployment }%%"  
+$deployment = "%%{ deployment }%%"
+$attacker_instances = "%%{ attacker_instances }%%"
+$target_instances = "%%{ target_instances }%%"
+$attacker_k8s_services = "%%{ attacker_k8s_services }%%"
+$target_k8s_services = "%%{ target_k8s_services }%%"
 
 echo "${environment}:${deployment}" > C:\\Windows\\Temp\\run_me.log
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${attacker_instances}")) | Out-File -FilePath C:\\Windows\\Temp\\attacker_instances.log -Append
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${target_instances}")) | Out-File -FilePath C:\\Windows\\Temp\\target_instances.log -Append
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${attacker_k8s_services}")) | Out-File -FilePath C:\\Windows\\Temp\\attacker_k8s_services.log -Append
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${target_k8s_services}")) | Out-File -FilePath C:\\Windows\\Temp\\target_k8s_services.log -Append
