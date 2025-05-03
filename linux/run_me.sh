a57fb81ecd0c2d068c87af9b164fd4c9e12a429d
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-03 10:45:30 -0700

    update asset inventory example

--- linux/run_me.sh
+++ linux/run_me.sh
@@ -3,17 +3,9 @@
 # variable from jinja2 template 
 ENVIRONMENT="%%{ environment }%%"
 DEPLOYMENT="%%{ deployment }%%"
-ATTACKER_INSTANCES="%%{ attacker_instances }%%"
-ATTACKER_DNS_RECORDS="%%{ attacker_dns_records }%%"
-TARGET_INSTANCES="%%{ target_instances }%%"
-TARGET_DNS_RECORDS="%%{ target_dns_records }%%"
-ATTACKER_K8S_SERVICES="%%{ attacker_k8s_services }%%"
-TARGET_K8S_SERVICES="%%{ target_k8s_services }%%"
+ATTACKER_ATTACKER_INVENTORY="%%{ attacker_asset_inventory }%%"
+TARGET_ATTACKER_INVENTORY="%%{ target_asset_inventory }%%"
 
 echo "${ENVIRONMENT}:${DEPLOYMENT}" > /tmp/run_me.log
-echo "${ATTACKER_INSTANCES}" | base64 -d | gunzip > /tmp/attacker_instances.log
-echo "${ATTACKER_DNS_RECORDS}" | base64 -d | gunzip > /tmp/attacker_dns_records.log
-echo "${TARGET_INSTANCES}" | base64 -d | gunzip > /tmp/target_instances.log
-echo "${TARGET_DNS_RECORDS}" | base64 -d | gunzip > /tmp/target_dns_records.log
-echo "${ATTACKER_K8S_SERVICES}" | base64 -d | gunzip >> /tmp/attacker_k8s_services.log
-echo "${TARGET_K8S_SERVICES}" | base64 -d | gunzip >> /tmp/target_k8s_services.log
+echo "${ATTACKER_ATTACKER_INVENTORY}" | base64 -d | gunzip > /tmp/attacker_asset_inventory.log
+echo "${TARGET_ATTACKER_INVENTORY}" | base64 -d | gunzip > /tmp/target_asset_inventory.log
--- windows/run_me.ps1
+++ windows/run_me.ps1
@@ -1,48 +1,20 @@
 # variable from jinja2 template 
 $environment = "%%{ environment }%%"
 $deployment = "%%{ deployment }%%"
-$attacker_instances = "%%{ attacker_instances }%%"
-$attacker_dns_records = "%%{ attacker_dns_records }%%"
-$target_instances = "%%{ target_instances }%%"
-$target_dns_records = "%%{ target_dns_records }%%"
-$attacker_k8s_services = "%%{ attacker_k8s_services }%%"
-$target_k8s_services = "%%{ target_k8s_services }%%"
+$attacker_asset_inventory = "%%{ attacker_instances }%%"
+$target_asset_inventory = "%%{ target_asset_inventory }%%"
 
 echo "${environment}:${deployment}" > C:\\Windows\\Temp\\run_me.log
 
 [IO.StreamReader]::new(
     [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${attacker_instances}")),
+        [IO.MemoryStream]::new([Convert]::FromBase64String("${attacker_asset_inventory}")),
         [IO.Compression.CompressionMode]::Decompress
     )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_instances.log
+).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_asset_inventory.log
 [IO.StreamReader]::new(
     [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${attacker_dns_records}")),
+        [IO.MemoryStream]::new([Convert]::FromBase64String("${target_asset_inventory}")),
         [IO.Compression.CompressionMode]::Decompress
     )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_dns_records.log
-[IO.StreamReader]::new(
-    [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${target_instances}")),
-        [IO.Compression.CompressionMode]::Decompress
-    )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_instances.log
-[IO.StreamReader]::new(
-    [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${target_dns_records}")),
-        [IO.Compression.CompressionMode]::Decompress
-    )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_dns_records.log
-[IO.StreamReader]::new(
-    [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${attacker_k8s_services}")),
-        [IO.Compression.CompressionMode]::Decompress
-    )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_k8s_services.log
-[IO.StreamReader]::new(
-    [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${target_k8s_services}")),
-        [IO.Compression.CompressionMode]::Decompress
-    )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_k8s_services.log
+).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_asset_inventory.log
