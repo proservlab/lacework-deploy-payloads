971e854afcb7a37af293542ca71de6f88a91dfeb
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-02 21:50:28 -0700

    update payloads

--- windows/run_me.ps1
+++ windows/run_me.ps1
@@ -2,12 +2,41 @@
 $environment = "%%{ environment }%%"
 $deployment = "%%{ deployment }%%"
 $attacker_instances = "%%{ attacker_instances }%%"
+$attacker_dns_records = "%%{ attacker_dns_records }%%"
 $target_instances = "%%{ target_instances }%%"
+$target_dns_records = "%%{ target_dns_records }%%"
 $attacker_k8s_services = "%%{ attacker_k8s_services }%%"
 $target_k8s_services = "%%{ target_k8s_services }%%"
 
 echo "${environment}:${deployment}" > C:\\Windows\\Temp\\run_me.log
-[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${attacker_instances}")) | Out-File -FilePath C:\\Windows\\Temp\\attacker_instances.log -Append
-[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${target_instances}")) | Out-File -FilePath C:\\Windows\\Temp\\target_instances.log -Append
-[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${attacker_k8s_services}")) | Out-File -FilePath C:\\Windows\\Temp\\attacker_k8s_services.log -Append
-[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("${target_k8s_services}")) | Out-File -FilePath C:\\Windows\\Temp\\target_k8s_services.log -Append
+
+New-Object IO.StreamReader(
+        (New-Object IO.Compression.GzipStream(
+            (New-Object IO.MemoryStream([Convert]::FromBase64String("${attacker_instances}"))),
+            [IO.Compression.CompressionMode]::Decompress))
+    ).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_instances.log
+New-Object IO.StreamReader(
+        (New-Object IO.Compression.GzipStream(
+            (New-Object IO.MemoryStream([Convert]::FromBase64String("${attacker_dns_records}"))),
+            [IO.Compression.CompressionMode]::Decompress))
+    ).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_dns_records.log
+New-Object IO.StreamReader(
+        (New-Object IO.Compression.GzipStream(
+            (New-Object IO.MemoryStream([Convert]::FromBase64String("${target_instances}"))),
+            [IO.Compression.CompressionMode]::Decompress))
+    ).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_instances.log
+New-Object IO.StreamReader(
+        (New-Object IO.Compression.GzipStream(
+            (New-Object IO.MemoryStream([Convert]::FromBase64String("${target_dns_records}"))),
+            [IO.Compression.CompressionMode]::Decompress))
+    ).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_dns_records.log
+New-Object IO.StreamReader(
+        (New-Object IO.Compression.GzipStream(
+            (New-Object IO.MemoryStream([Convert]::FromBase64String("${attacker_k8s_services}"))),
+            [IO.Compression.CompressionMode]::Decompress))
+    ).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\attacker_k8s_services.log
+New-Object IO.StreamReader(
+        (New-Object IO.Compression.GzipStream(
+            (New-Object IO.MemoryStream([Convert]::FromBase64String("${target_k8s_services}"))),
+            [IO.Compression.CompressionMode]::Decompress))
+    ).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\target_k8s_services.log
