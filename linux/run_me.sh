6ba99f10d31788c978123fa5322f0ddca29e641a
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-06 13:03:42 -0700

    update template var reference

--- linux/run_me.sh
+++ linux/run_me.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # variable from jinja2 template 
-ENVIRONMENT="%%{ environment }%%"
-DEPLOYMENT="%%{ deployment }%%"
-ATTACKER_ATTACKER_INVENTORY="%%{ attacker_asset_inventory }%%"
-TARGET_ATTACKER_INVENTORY="%%{ target_asset_inventory }%%"
+ENVIRONMENT="${ environment }"
+DEPLOYMENT="${ deployment }"
+ATTACKER_ATTACKER_INVENTORY="${ attacker_asset_inventory }"
+TARGET_ATTACKER_INVENTORY="${ target_asset_inventory }"
 
-echo "${ENVIRONMENT}:${DEPLOYMENT}" > /tmp/run_me.log
-echo "${ATTACKER_ATTACKER_INVENTORY}" | base64 -d | gunzip > /tmp/attacker_asset_inventory.log
-echo "${TARGET_ATTACKER_INVENTORY}" | base64 -d | gunzip > /tmp/target_asset_inventory.log
+echo "$${ENVIRONMENT}:$${DEPLOYMENT}" > /tmp/run_me.log
+echo "$${ATTACKER_ATTACKER_INVENTORY}" | base64 -d | gunzip > /tmp/attacker_asset_inventory.log
+echo "$${TARGET_ATTACKER_INVENTORY}" | base64 -d | gunzip > /tmp/target_asset_inventory.log
