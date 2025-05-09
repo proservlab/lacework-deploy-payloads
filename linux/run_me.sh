52924abf54583bc39ab7487e1dd24aeb24993520
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-09 11:49:45 -0700

    add context parsing example

--- linux/run_me.sh
+++ linux/run_me.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-
+####################################################################
+# Example script to demonstrate how to use environment context in a bash script
+######################################################################
 # environment context 
+if [ -z "$ENV_CONTEXT" ]; then
+    echo "ENV_CONTEXT is not set. Exiting."
+    exit 1
+fi
+
 ENV_CONTENT="$(echo $ENV_CONTEXT | base64 -d | gunzip)"
 
+# get jq if we don't have it
+if ! command -v jq; then
+    if [ uname -a | grep -q "Linux" ]; then
+        if command -v apt-get; then
+            apt-get update && apt-get install -y jq
+        elif command -v yum; then
+            yum install -y jq
+        fi
+    fi
+fi
+
+$environment = $(echo $ENV_CONTEXT | jq -r '.environment')
+$deployment = $(echo $ENV_CONTEXT | jq -r '.deployment')
+$attacker_asset_inventory = $(echo $ENV_CONTEXT | jq -r '.attacker_asset_inventory' | base64 -d | gunzip)
+$target_asset_inventory = $(echo $ENV_CONTEXT | jq -r '.target_asset_inventory' | base64 -d | gunzip)
+
+cat <<EOF > /tmp/run_me.log
+environment: $environment
+deployment: $deployment
+attacker_asset_inventory: $attacker_asset_inventory
+target_asset_inventory: $target_asset_inventory
+EOF
 echo "${ENV_CONTENT}" > /tmp/run_me.log
--- windows/run_me.ps1
+++ windows/run_me.ps1
@@ -1,9 +1,31 @@
-# variable from jinja2 template 
+####################################################################
+# Example script to demonstrate how to use environment context in a bash script
+######################################################################
+
+if ($env:ENV_CONTEXT -eq $null) {
+    Write-Host "Environment context is not set."
+    exit 1
+}
+
 $env_context_compressed = "$env:ENV_CONTEXT"
 
-[IO.StreamReader]::new(
-    [IO.Compression.GzipStream]::new(
-        [IO.MemoryStream]::new([Convert]::FromBase64String("${env_context_compressed}")),
-        [IO.Compression.CompressionMode]::Decompress
-    )
-).ReadToEnd() | Out-File -FilePath C:\\Windows\\Temp\\run_me.log
+function Get-Base64GzipString($input) {
+    return [IO.StreamReader]::new(
+        [IO.Compression.GzipStream]::new(
+            [IO.MemoryStream]::new([Convert]::FromBase64String("${env_context_compressed}")),
+            [IO.Compression.CompressionMode]::Decompress
+        )).ReadToEnd()
+}
+
+$env_context = Get-Base64GzipString($env_context_compressed) | ConvertFrom-Json
+$deployment = $env_context["deployment"]
+$environment = $env_context["environment"]
+$attacker_asset_inventory = Get-Base64GzipString($env_context["attacker_asset_inventory"])
+$target_asset_inventory = Get-Base64GzipString($env_context["target_asset_inventory"])
+
+$output = @{
+    deployment = $deployment
+    environment = $environment
+    attacker_asset_inventory = $attacker_asset_inventory
+    target_asset_inventory = $target_asset_inventory
+} | ConvertTo-Json -Depth | Out-File -FilePath C:\\Windows\\Temp\\run_me.log
