34b7295e236d82388fbbc06c173dc29f60ea7723
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-10 12:20:04 -0700

    add payload example

--- linux/common.sh
+++ linux/common.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
 get_base64gzip() {
   local payload="$1"
   echo $payload | base64 -d | gunzip
--- linux/run_me.sh
+++ linux/run_me.sh
@@ -5,8 +5,8 @@
 
 URL="https://raw.githubusercontent.com/proservlab/lacework-deploy-payloads/main/linux/common.sh"
 FUNC=$(mktemp)
-curl -LJ -s $URL > $FUNC
-
+echo $FUNC
+curl -LJ -s $URL -o $FUNC
 # make the function available in the current shell
 if [ $? -ne 0 ]; then
     echo "Failed to download the function. Exiting."
@@ -23,29 +23,41 @@ if [ -z "$ENV_CONTEXT" ]; then
     exit 1
 fi
 
-ENV_CONTENT="$(get_base64gzip $ENV_CONTEXT)"
+ENV_JSON="$(get_base64gzip $ENV_CONTEXT)"
 
 # get jq if we don't have it
-if ! command -v jq; then
-    if [ uname -a | grep -q "Linux" ]; then
-        if command -v apt-get; then
+if ! command_exists "jq"; then
+    if [[ $(uname -s) == "Linux" ]]; then
+        if command_exists "apt-get"; then
             apt-get update && apt-get install -y jq
-        elif command -v yum; then
+        elif command_exists "yum"; then
             yum install -y jq
         fi
+    elif [[ $(uname -s) == "Darwin" ]]; then
+        if command_exists "brew"; then
+            brew install jq
+        else
+            echo "jq is not installed and brew is not available. Exiting."
+            rm -f $FUNC
+            exit 1
+        fi
+    else
+        echo "Unsupported OS. Exiting."
+        rm -f $FUNC
+        exit 1
     fi
 fi
 
-environment=$(echo $ENV_CONTEXT | jq -r '.environment')
-deployment=$(echo $ENV_CONTEXT | jq -r '.deployment')
-attacker_asset_inventory=$(echo $ENV_CONTEXT | jq -r '.attacker_asset_inventory' | get_base64gzip)
-target_asset_inventory=$(echo $ENV_CONTEXT | jq -r '.target_asset_inventory' | get_base64gzip)
-attacker_lacework_agent_access_token=$(echo $ENV_CONTEXT | jq -r '.attacker_lacework_agent_access_token' | get_base64gzip)
-attacker_lacework_server_url=$(echo $ENV_CONTEXT | jq -r '.attacker_lacework_server_url' | get_base64gzip)
-target_lacework_agent_access_token=$(echo $ENV_CONTEXT | jq -r '.target_lacework_agent_access_token' | get_base64gzip)
-target_lacework_server_url=$(echo $ENV_CONTEXT | jq -r '.target_lacework_server_url' | get_base64gzip)
+environment=$(echo $ENV_JSON | jq -r '.environment')
+deployment=$(echo $ENV_JSON | jq -r '.deployment')
+attacker_asset_inventory=$(get_base64gzip $(echo $ENV_JSON | jq -r '.attacker_asset_inventory'))
+target_asset_inventory=$(get_base64gzip $(echo $ENV_JSON | jq -r '.target_asset_inventory'))
+attacker_lacework_agent_access_token=$(echo $ENV_JSON | jq -r '.attacker_lacework_agent_access_token')
+attacker_lacework_server_url=$(echo $ENV_JSON | jq -r '.attacker_lacework_server_url')
+target_lacework_agent_access_token=$(echo $ENV_JSON | jq -r '.target_lacework_agent_access_token')
+target_lacework_server_url=$(echo $ENV_JSON | jq -r '.target_lacework_server_url')
 
-cat <<EOF > /tmp/run_me.log
+cat <<EOF | tee /tmp/run_me.log
 environment: $environment
 deployment: $deployment
 attacker_asset_inventory: $attacker_asset_inventory
@@ -55,5 +67,4 @@ attacker_lacework_server_url: $attacker_lacework_server_url
 target_lacework_agent_access_token: $target_lacework_agent_access_token
 target_lacework_server_url: $target_lacework_server_url
 EOF
-echo "${ENV_CONTENT}" > /tmp/run_me.log
 rm -f $FUNC
