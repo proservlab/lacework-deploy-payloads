7f88d11d199f5759d9e2a50c9c30ce7c7edb46cf
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-11 17:25:08 -0700

    add payload example

--- windows/run_me.ps1
+++ windows/run_me.ps1
@@ -58,21 +58,21 @@ try {
     #################################################################
 
     $env_context_compressed = "$env:ENV_CONTEXT"
-    $env_context = Get-Base64GzipString -Base64Payload $env_context_compressed | ConvertFrom-Json
-    Write-Log-Message "Environment context: $($env_context | ConvertTo-Json -Compress)"
+    $env_context = Get-Base64GzipString -Base64Payload $env_context_compressed | ConvertFrom-Json -Depth 10
+    Write-Log-Message "Environment context: $($env_context | ConvertTo-Json -Compress -Depth 10)"
     Write-Log-Message "Building full payload..."
     @{
         "env_context"                          = $env_context
         "tag"                                  = $env:TAG
         "deployment"                           = $env_context.deployment
         "environment"                          = $env_context.environment
-        "attacker_asset_inventory"             = Get-Base64GzipString -Base64Payload $env_context.attacker_asset_inventory | ConvertFrom-Json
-        "target_asset_inventory"               = Get-Base64GzipString -Base64Payload $env_context.target_asset_inventory | ConvertFrom-Json
+        "attacker_asset_inventory"             = Get-Base64GzipString -Base64Payload $env_context.attacker_asset_inventory | ConvertFrom-Json -Depth 10
+        "target_asset_inventory"               = Get-Base64GzipString -Base64Payload $env_context.target_asset_inventory | ConvertFrom-Json -Depth 10
         "attacker_lacework_agent_access_token" = $env_context.attacker_lacework_agent_access_token
         "attacker_lacework_server_url"         = $env_context.attacker_lacework_server_url
         "target_lacework_agent_access_token"   = $env_context.target_lacework_agent_access_token
         "target_lacework_server_url"           = $env_context.target_lacework_server_url
-    } | ConvertTo-Json -Compress | Out-File -FilePath $env:TEMP\\run_me.log
+    } | ConvertTo-Json -Compress | Out-File -FilePath $env:TEMP\\run_me_env_context.log
     Write-Log-Message "Payload built and logged to $env:TEMP\\run_me.log"
 }
 catch {
