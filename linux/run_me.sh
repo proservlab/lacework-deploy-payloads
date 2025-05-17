6cdf1d1a01a101c23199b75b6d93d21a021c4a87
credibleforce <58786780+credibleforce@users.noreply.github.com>
2025-05-17 08:41:20 -0700

    update examples

--- linux/run_me.sh
+++ linux/run_me.sh
@@ -3,6 +3,18 @@
 # Example script to demonstrate how to use environment context in a bash script
 ######################################################################
 
+# make sure we have the required packages installed
+if command -v "apt-get" >/dev/null 2>&1; then
+    export PACKAGE_MANAGER="apt-get"
+    export RETRY="-o Acquire::Retries=10"
+elif command -v "yum" >/dev/null 2>&1; then
+    export PACKAGE_MANAGER="yum"
+    export RETRY="--setopt=retries=10"
+fi
+
+echo "running: $PACKAGE_MANAGER update && $PACKAGE_MANAGER install -y curl jq openssl procps"
+$PACKAGE_MANAGER update && $PACKAGE_MANAGER install -y curl jq openssl procps
+
 URL="https://raw.githubusercontent.com/proservlab/lacework-deploy-payloads/main/linux/common.sh"
 FUNC=$(mktemp)
 curl -LJ -s $URL -o $FUNC
